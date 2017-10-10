636a44c054cc94f3d106922dc81c3513e95da06b
Keanu Attema <info.keanu.attema@gmail.com>
2017-10-10 12:57:54 +0200

    Fixed Dev branche

--- Memory/MainPage.xaml.cs
+++ Memory/MainPage.xaml.cs
@@ -41,12 +41,12 @@ namespace Memory
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    ButtonArray[i].Button = new Button() { Content = i + j, Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255,155,155,155)),  Width = 125, Height = 125, };
-                    ButtonArray[i].Button.Click += new RoutedEventHandler(this.ClickedCard);
+                    ButtonArray[i+j].Button = new Button() { Content = i + j, Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255,155,155,155)),  Width = 125, Height = 125, };
+                    ButtonArray[i+j].Button.Click += new RoutedEventHandler(this.ClickedCard);
 
-                    ButtonArray[i].Type = Types[i + j];
-                    ButtonArray[i].Button.Content = i;
-                    Memory_Grid.Children.Add(ButtonArray[i].Button);
+                    ButtonArray[i+j].Type = Types[i + j];
+                    ButtonArray[i+j].Button.Content = i;
+                    Memory_Grid.Children.Add(ButtonArray[i+j].Button);
 
                     Grid.SetRow(ButtonArray[i].Button, i);
                     Grid.SetColumn(ButtonArray[i].Button, j);
@@ -56,7 +56,14 @@ namespace Memory
 
         public void ClickedCard(object sender, RoutedEventArgs e)
         {
-
+            for (int i = 0; i < 16; i++)
+            {
+                if(sender == ButtonArray[i].Button)
+                {
+                    ButtonArray[i].Button.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 76, 103));
+                    return;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
