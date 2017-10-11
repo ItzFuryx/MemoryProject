a1d1374595fb20f4218eeb15aea122d7f1b94962
Keanu Attema <info.keanu.attema@gmail.com>
2017-10-11 14:34:08 +0200

    fixed project

---
+++ MemoryGame/MainPage.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MemoryGame
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.StartGameButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GridPanel
+            // 
+            this.GridPanel.Location = new System.Drawing.Point(396, 65);
+            this.GridPanel.Name = "GridPanel";
+            this.GridPanel.Size = new System.Drawing.Size(541, 585);
+            this.GridPanel.TabIndex = 0;
+            // 
+            // StartGameButton
+            // 
+            this.StartGameButton.Location = new System.Drawing.Point(493, 161);
+            this.StartGameButton.Name = "StartGameButton";
+            this.StartGameButton.Size = new System.Drawing.Size(148, 58);
+            this.StartGameButton.TabIndex = 1;
+            this.StartGameButton.Text = "Start Game";
+            this.StartGameButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(184, 65);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(148, 58);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.MainPanel.Controls.Add(this.StartGameButton);
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1267, 720);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.GridPanel);
+            this.Name = "MainPage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel GridPanel;
+        private System.Windows.Forms.Button StartGameButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Panel MainPanel;
+    }
+}
+
