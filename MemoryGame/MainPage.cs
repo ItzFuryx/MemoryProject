593abb3aebeefcfa0913585279d9bb8e02e7cbca
Keanu Attema <info.keanu.attema@gmail.com>
2017-10-12 12:08:52 +0200

    fixed delay yet a new bug the function FixCards doesnt fully run

--- MemoryGame/MainPage.cs
+++ MemoryGame/MainPage.cs
@@ -6,18 +6,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MemoryGame
 {
     public partial class MainPage : Form
     {
-        private const int MemoryItems = 16;
-        public int Sets = 0;
-        MemoryButton[] ButtonArray = new MemoryButton[MemoryItems];
-        MemoryButton FirstButton;
+        internal const int MemoryItems = 16;
+        internal int Sets = 0;
+        internal MemoryButton[] ButtonArray = new MemoryButton[MemoryItems];
+        internal MemoryButton FirstButton;
+        internal MemoryButton SecondButton;
         
-        MemoryType[] Types = new MemoryType[MemoryItems];
+        internal MemoryType[] Types = new MemoryType[MemoryItems];
 
         public MainPage()
         {
@@ -73,7 +75,6 @@ namespace MemoryGame
             SetsLabel.Visible = false;
         }
         
-        //bart
         public void ClickedCard(object sender, EventArgs e)
         {      
             for (int i = 0; i < MemoryItems; i++)
@@ -82,47 +83,48 @@ namespace MemoryGame
                 {
                     ButtonArray[i].Button.BackColor = Color.MediumVioletRed;
                     ButtonArray[i].Button.Text = ButtonArray[i].Type.ToString();
-                    
+
                     if (FirstButton == null)
                     {
                         FirstButton = ButtonArray[i];
                     }
                     else if(FirstButton != null)
                     {
-                        MemoryButton secondButton = ButtonArray[i];
+                        SecondButton = ButtonArray[i];
 
-                        System.Threading.Thread.Sleep(1000);
-                        if (FirstButton.Type == secondButton.Type)
-                        {
-                            FirstButton.Button.BackColor = Color.Yellow;
-                            secondButton.Button.BackColor = Color.Yellow;                            
-                        }
-                        else
-                        {
-                            FirstButton.Button.BackColor = Color.Gray;
-                            FirstButton.Button.Text = null;
-                            secondButton.Button.BackColor = Color.Gray;
-                            secondButton.Button.Text = null;
-                        }
-                        
-                        FirstButton = null;
+                        var t = Task.Run(async delegate
+                                {
+                                    await Task.Delay(1000);
+                                    FixCards();
+                                });
                     }
                     SetsLabel.Text = ((Sets+1)/2).ToString();
                     Sets++;
                      break;
                 }
+            }            
+        }
+
+        void FixCards()
+        {
+            if (FirstButton.Type == SecondButton.Type)
+            {
+                FirstButton.Button.BackColor = Color.Yellow;
+                SecondButton.Button.BackColor = Color.Yellow;
             }
-            
+            else
+            {
+                SecondButton.Button.BackColor = Color.Gray;
+                SecondButton.Button.Text = string.Empty;
+                FirstButton.Button.BackColor = Color.Gray;
+                FirstButton.Button.Text = string.Empty;
+            }
+            FirstButton = null;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        } 
     }
 }
